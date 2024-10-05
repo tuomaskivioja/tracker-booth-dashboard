797d2c8a508da5374f03fba3ba4a9cad6a82adc5
Tuomas <tuomas@internetmadecoder.com>
2024-10-05 10:49:55 +0400

    key

--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ import reportWebVitals from './reportWebVitals';
 import { ClerkProvider } from '@clerk/clerk-react'
 
 // Import your publishable key
-const PUBLISHABLE_KEY = process.env.REACT_APP_CLERK_PUBLISHABLE_KEY;
+// const PUBLISHABLE_KEY = process.env.REACT_APP_CLERK_PUBLISHABLE_KEY;
+const PUBLISHABLE_KEY = import.meta.env.REACT_APP_CLERK_PUBLISHABLE_KEY;
 
 if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key")
