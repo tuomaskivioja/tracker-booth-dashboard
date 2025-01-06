7098018b186fd46292ec86bbb2625ac13d1ddf57
Tuomas <tuomas@internetmadecoder.com>
2025-01-06 11:59:29 +0400

    a

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Dashboard from './components/Dashboard/Dashboard';
 import Sidebar from './components/Sidebar/Sidebar';
 import Code from './components/Code/Code';
@@ -7,8 +7,79 @@ import { BrowserRouter as Router, Route, Routes, Navigate, useLocation } from 'r
 import { SignedIn, SignedOut, SignOutButton, useUser } from "@clerk/clerk-react";
 import SignInPage from './components/SignIn/SignIn';
 import Activity from './components/Activity/Activity';
+import { useRevit } from './contexts/RevitContext'
+
 function App() {
+    const SERVER_URL = process.env.REACT_APP_SERVER_URL;
     const { isSignedIn, user, isLoaded } = useUser();
+    const { username } = useRevit();
+    const [isSubscribed, setIsSubscribed] = useState(false);
+    const [subscriptionExpiresAt, setSubscriptionExpiresAt] = useState(null);
+
+    // useEffect(() => {
+    //     const checkSubscription = async () => {
+    //         console.log(subscriptionExpiresAt)
+    //         if (subscriptionExpiresAt) {
+    //           const now = new Date();
+    //           const expiresAt = new Date(subscriptionExpiresAt);
+
+    //           console.log(expiresAt, now)
+
+    //           if (expiresAt > now) {
+    //             setIsSubscribed(true);
+    //             return;
+    //           } else {
+    //             setIsSubscribed(false);
+    //             createCheckoutSession()
+    //           }
+    //         }
+    //         if (isSignedIn && user && username) {
+    //             try {
+    //                 const response = await fetch(`https://${SERVER_URL}/api/check-subscription/${username}`, {
+    //                     method: 'GET',
+    //                     headers: {
+    //                         'Content-Type': 'application/json',
+    //                     },
+    //                 });
+
+    //                 if (!response.ok) {
+    //                     throw new Error('Failed to check subscription status');
+    //                 }
+
+    //                 const subscriptionData = await response.json();
+    //                 setIsSubscribed(subscriptionData.isSubscribed);
+    //                 setSubscriptionExpiresAt(subscriptionData.subscriptionExpiresAt);
+
+    //                 console.log('Subscription status:', subscriptionData);
+    //             } catch (error) {
+    //                 console.error('Error checking subscription status:', error);
+    //             }
+    //         }
+    //     };
+
+    //     checkSubscription();
+    // }, [isSignedIn, user, username, subscriptionExpiresAt, isSubscribed]);
+
+    // const createCheckoutSession = async () => {
+    //     try {
+    //         const response = await fetch(`https://${SERVER_URL}/api/create-checkout-session`, {
+    //             method: 'POST',
+    //             headers: {
+    //                 'Content-Type': 'application/json',
+    //             },
+    //             body: JSON.stringify({ userId: username }),
+    //         });
+
+    //         if (!response.ok) {
+    //             throw new Error('Failed to create checkout session');
+    //         }
+
+    //         const { url } = await response.json();
+    //         window.location.href = url;
+    //     } catch (error) {
+    //         console.error('Error creating checkout session:', error);
+    //     }
+    // };
 
     return (
       <Router>
@@ -21,16 +92,24 @@ function App() {
             </Routes>
           </SignedOut>
           <SignedIn>
-            <Sidebar />
-            <div className="content">
-              <Routes>
-                <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-                <Route path="/" element={<Navigate to="/dashboard" />} />
-                <Route path="/dashboard" element={<Dashboard />} />
-                <Route path="/code" element={<Code />} />
-                <Route path="/activity" element={<Activity />} />
-              </Routes>
-            </div>
+            {/* {isSubscribed ? (
+              <> */}
+                <Sidebar />
+                <div className="content">
+                  <Routes>
+                    <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+                    <Route path="/" element={<Navigate to="/dashboard" />} />
+                    <Route path="/dashboard" element={<Dashboard />} />
+                    <Route path="/code" element={<Code />} />
+                    <Route path="/activity" element={<Activity />} />
+                  </Routes>
+                </div>
+              {/* </>
+            ) : (
+              <div>
+                <p></p>
+              </div>
+            )} */}
           </SignedIn>
         </header>
       </Router>
--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -3,7 +3,34 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './Sidebar.css'; // Importing CSS for Sidebar
 import { SignOutButton } from '@clerk/clerk-react';
+import { useRevit } from '../../contexts/RevitContext';
+
+
 const Sidebar = () => {
+    const { username } = useRevit();
+    const SERVER_URL = process.env.REACT_APP_SERVER_URL;
+    
+    const openBillingPortal = async () => {
+        try {
+            const response = await fetch(`https://${SERVER_URL}/api/create-billing-portal-session`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ userId: username }), // Replace 'currentUserId' with actual user ID
+            });
+
+            if (!response.ok) {
+                throw new Error('Failed to create billing portal session');
+            }
+
+            const { url } = await response.json();
+            window.location.href = url;
+        } catch (error) {
+            console.error('Error opening billing portal:', error);
+        }
+    };
+
     return (
         <div className="sidebar">
             <h2>Menu</h2>
@@ -23,10 +50,15 @@ const Sidebar = () => {
                         Conversion activity
                     </NavLink>
                 </li>
+                <li>
+                    <button onClick={openBillingPortal} className="billing-portal-button">
+                        Manage subscription
+                    </button>
+                </li>
             </ul>
             <SignOutButton displayName="Sign out from Revit" style={{ backgroundColor: '#e74c3c' }} />
             <div className="footer">
-                © 2024 MyApp
+                © 2025 Revit
             </div>
         </div>
     );
